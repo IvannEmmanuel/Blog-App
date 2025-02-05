3b709818c4c8a012b70c7d1e425a9c88e1f405a7
IvannEmmanuel <F0dG1ngD13@gmail.com>
2025-02-05 14:55:22 +0800

    Added Server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.use('/', (req, res) => {
+    res.send("Server is running.");
+})
+
+const port = 5000
+
+app.listen(port, console.log(`Server started on port ${port}`))
